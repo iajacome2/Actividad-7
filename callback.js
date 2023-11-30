824df3fb03de134d259661ba1f4513d3b355b861
Ivonne Alexandra Jacome Sangoluisa <iajacome2@espe.edu.ec>
2023-11-30 01:08:41 -0500

    actividad-7

---
+++ callback.js
@@ -0,0 +1,40 @@
+class CounterComponent extends HTMLElement {
+    constructor() {
+        super();
+        this._counter = 0;
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <div class="container mt-4">
+          <div class="text-center">
+            <p class="display-4">Count: ${this._counter}</p>
+            <button id="increment" class="btn btn-primary">SUMA</button>
+            <button id="decrement" class="btn btn-success">RESTA</button>
+          </div>
+        </div>
+      `;
+
+        this.attachListeners();
+    }
+
+    attachListeners() {
+        this.shadowRoot.querySelector('#increment').addEventListener('click', () => {
+            this._counter++;
+            this.render();
+        });
+
+        this.shadowRoot.querySelector('#decrement').addEventListener('click', () => {
+            this._counter--;
+            this.render();
+        });
+    }
+}
+
+customElements.define('counter-component', CounterComponent);
